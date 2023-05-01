97df13d158edef35ea68325eebddd16ab30837fb
iscodand <iscodand@outlook.com>
2023-05-01 17:05:35 -0300

    add: mapping profiles with auto mapper

---
+++ PokemonReview/Helpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PokemonReview.Data.DTOs;
+using PokemonReview.Models;
+
+namespace PokemonReview.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDTO>();
+        }
+    }
+}
