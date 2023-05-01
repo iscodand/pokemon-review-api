82c965b0ed6092d2b59c7691af2547cba7c17722
iscodand <iscodand@outlook.com>
2023-05-01 17:05:10 -0300

    add: migrations folder

---
+++ PokemonReview/Migrations/20230501163235_RemoveIdFromJoinTables.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReview.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIdFromJoinTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "PokemonCategories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "PokemonOwners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "PokemonCategories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
