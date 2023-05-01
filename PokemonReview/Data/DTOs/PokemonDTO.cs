0a7ae21b0e1a69165411c2cdd4ea48f8b341663e
iscodand <iscodand@outlook.com>
2023-05-01 17:04:56 -0300

    add: first Pokemon DTO

---
+++ PokemonReview/Data/DTOs/PokemonDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReview.Data.DTOs
+{
+    public class PokemonDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
