c95d0138fd0a11c081a1d5618cd5bea1037b16b0
iscodand <iscodand@outlook.com>
2023-05-01 13:07:39 -0300

    add: many-to-many relationships

--- PokemonReview/Data/DataContext.cs
+++ PokemonReview/Data/DataContext.cs
@@ -16,5 +16,30 @@ namespace PokemonReview.Data
         public DbSet<Country> Countries { get; set; }
         public DbSet<PokemonCategory> PokemonCategories { get; set; }
         public DbSet<PokemonOwner> PokemonOwners { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(po => po.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(po => po.PokemonOwners)
+                .HasForeignKey(o => o.OwnerId);
+
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(pc => PokemonCategories)
+                .HasForeignKey(c => c.CategoryId);
+        }
     }
 }
