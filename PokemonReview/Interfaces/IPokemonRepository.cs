2bfb6d40c3dd077b405c56c23a624b0cbcc6988f
iscodand <iscodand@outlook.com>
2023-05-01 17:06:00 -0300

    add: repository pattern with Pokemon Interface and Repository

---
+++ PokemonReview/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReview.Models;
+
+namespace PokemonReview.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        public ICollection<Pokemon> GetPokemons();
+        public Pokemon GetPokemon(int pokeId);
+        public Pokemon GetPokemon(string name);
+        public decimal GetPokemonRating(int pokeId);
+        public bool GetPokemonExists(int pokeId);
+    }
+}
---
+++ PokemonReview/Repository/PokemonRepository.cs
@@ -0,0 +1,49 @@
+﻿using PokemonReview.Data;
+using PokemonReview.Interfaces;
+using PokemonReview.Models;
+
+namespace PokemonReview.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public Pokemon GetPokemon(int pokeId)
+        {
+            return _context.Pokemons.Where(p => p.Id == pokeId).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var reviews = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+            int totalReviews = reviews.Count();
+
+            if (totalReviews <= 0)
+                return 0;
+
+            decimal rating = ((decimal)reviews.Sum(r => r.Rating)) / totalReviews;
+
+            return Math.Round(rating, 1);
+        }
+
+        public bool GetPokemonExists(int pokeId)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokeId);
+        }
+    }
+}
