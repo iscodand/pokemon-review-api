9745acf043f557c6fabb3e66894a0d86fc38f185
iscodand <iscodand@outlook.com>
2023-05-01 11:22:18 -0300

    add: initial db context

---
+++ PokemonReview/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReview.Models;
+using System;
+
+namespace PokemonReview.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+    }
+}
