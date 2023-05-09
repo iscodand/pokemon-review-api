d0c8b44c09bd05bcfe5ba79a918c9048b9e3e8af
iscodand <iscodand@outlook.com>
2023-05-09 14:16:36 -0300

    add: Register User DTO's

---
+++ UserAuth.Api/Data/DTOs/Request/RegisterUserDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAuth.Api.Data.DTOs.Request
+{
+    public class RegisterUserDTO
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(32, MinimumLength = 3)]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "E-mail is required.")]
+        [StringLength(90, MinimumLength = 3)]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(90, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Password confirm is required.")]
+        [StringLength(90, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password doens't match password.")]
+        public string? PasswordConfirm { get; set; }
+    }
+}
---
+++ UserAuth.Api/Data/DTOs/Response/UserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAuth.Api.Data.DTOs.Response
+{
+    public class UserResponse
+    {
+        public string? Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public IEnumerable<string>? ErrorMessages { get; set; }
+    }
+}
