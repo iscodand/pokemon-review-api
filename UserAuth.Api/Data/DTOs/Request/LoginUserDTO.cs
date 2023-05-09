995e1287438405a0221cf3999f6d9e68fe985cc1
iscodand <iscodand@outlook.com>
2023-05-09 14:16:23 -0300

    add: Login DTO's

---
+++ UserAuth.Api/Data/DTOs/Request/LoginUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace UserAuth.Api.Data.DTOs.Request
+{
+    public class LoginUserDTO
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ UserAuth.Api/Data/DTOs/Response/TokenResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace UserAuth.Api.Data.DTOs.Response
+{
+    public class TokenResponse
+    {
+        public string Message { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public bool IsSuccess { get; set; }
+        public ICollection<string> ErrorMessages { get; set; }
+        public DateTime? ExpirationTime { get; set; }
+    }
+}
