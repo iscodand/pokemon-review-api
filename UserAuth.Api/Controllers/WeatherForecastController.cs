93fb0a14c2be7ed93762a6a0bbd3ff8eacd62a40
iscodand <iscodand@outlook.com>
2023-05-09 14:15:45 -0300

    add: WeatherForecast for test Authorization

---
+++ UserAuth.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1;
+
+namespace AspNetIdentityDemo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier);
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
