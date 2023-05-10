c2def82400fd561688de6b807c6ca51376b15cf3
iscodand <iscodand@outlook.com>
2023-05-10 11:30:59 -0300

    add: TokenRepository interface

---
+++ UserAuth.Api/Interfaces/ITokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace UserAuth.Api.Interfaces
+{
+    public interface ITokenRepository
+    {
+        public string GenerateRefreshToken();
+        public JwtSecurityToken GenerateAccessToken(List<Claim> claims);
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string? token);
+    }
+}
