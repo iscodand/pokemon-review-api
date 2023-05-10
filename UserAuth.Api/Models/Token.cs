ae821ee729a3281a071ec2336debc6cf10ee8f5b
iscodand <iscodand@outlook.com>
2023-05-10 11:31:48 -0300

    add: custom models for user and tokens

---
+++ UserAuth.Api/Models/Token.cs
@@ -0,0 +1,8 @@
+﻿namespace UserAuth.Api.Models
+{
+    public class Token
+    {
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
---
+++ UserAuth.Api/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserAuth.Api.Models
+{
+    public class User : IdentityUser
+    {
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
