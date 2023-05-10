594f5c904a25b2b80d3f95ff843a3676054f05bd
iscodand <iscodand@outlook.com>
2023-05-10 11:30:36 -0300

    update: add fields for IdentityUser Model

--- UserAuth.Api/Data/DataContext.cs
+++ UserAuth.Api/Data/DataContext.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UserAuth.Api.Models;
 
 namespace UserAuth.Api.Data
 {
-    public class DataContext : IdentityDbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
